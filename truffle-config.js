1e21a1fd9e14f6f0e97d5a22e1bc79b5c04cfae4
Jean-François Prince <jeanfrancois.prince@gmail.com>
2019-05-08 23:06:15 +0200

    Add files via upload

---
+++ truffle-config.js
@@ -0,0 +1,46 @@
+// Allows us to use ES6 in our migrations and tests.
+require('babel-register')
+
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const mnemonic = "surround inflict property BLBL";
+const infuraAPIKey = "2153v4ds354fvds3543";
+
+module.exports = {
+   networks: { 
+    ganache: {
+    development: {
+    host: "127.0.0.1",
+    port: 9545,
+    network_id: "*" // Match any network id
+    }
+  },
+  rinkeby: {
+    provider: function() {
+      return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/" + infuraAPIKey);
+    },
+    network_id: '4',
+    gas: 0,
+    gasPrice: 0,
+  }
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.5.2",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
+  }
+}
